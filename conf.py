69417c29b6515d98f0f95d5f5260bffc3676dabd
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-07-25 10:14:46 +0800

    配置文件

---
+++ conf.py
@@ -0,0 +1,5 @@
+CAMERA_IP = "192.168.1.100"
+SDK_PATH = "/mnt/HD_2TB/hua/github/hk_face/hk_sdk"
+CAMERA_NAME = "admin"
+CAMERA_PWD = "!QAZ2wsx3edc"
+IMG_SAVE_PATH = "/mnt/HD_2TB/hua/github/hk_face/face"
